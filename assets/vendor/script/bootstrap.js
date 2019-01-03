73845c451ae02a60c16d1fb4df38d9b8374257bd
Katon Aditya <tonn.aditya@gmail.com>
2019-01-03 22:30:35 +0700

    Add files via upload

---
+++ assets/vendor/script/bootstrap.js
@@ -0,0 +1,208 @@
+var a = document.getElementById("myAudio");
+function playAudio() {
+    a.play();
+}
+var aa = document.getElementById("myAudio1");
+function playAudio1() {
+    aa.play();
+}
+var b = document.getElementById("myAudio2");
+function playAudio2() {
+    b.play();
+}
+var bb = document.getElementById("myAudio3");
+function playAudio3() {
+    bb.play();
+}
+var c = document.getElementById("myAudio4");
+function playAudio4() {
+    c.play();
+}
+var cc = document.getElementById("myAudio5");
+function playAudio5() {
+    cc.play();
+}
+var d = document.getElementById("myAudio6");
+function playAudio6() {
+    d.play();
+}
+var dd = document.getElementById("myAudio7");
+function playAudio7() {
+    dd.play();
+}
+var e = document.getElementById("myAudio8");
+function playAudio8() {
+    e.play();
+}
+var ee = document.getElementById("myAudio9");
+function playAudio9() {
+    ee.play();
+}
+var f = document.getElementById("myAudio10");
+function playAudio10() {
+    f.play();
+}
+var ff = document.getElementById("myAudio11");
+function playAudio11() {
+    ff.play();
+}
+var g = document.getElementById("myAudio12");
+function playAudio12() {
+    g.play();
+}
+var gg = document.getElementById("myAudio13");
+function playAudio13() {
+    gg.play();
+}
+var h = document.getElementById("myAudio14");
+function playAudio14() {
+    h.play();
+}
+var hh = document.getElementById("myAudio15");
+function playAudio15() {
+    hh.play();
+}
+var i = document.getElementById("myAudio16");
+function playAudio16() {
+    i.play();
+}
+var ii = document.getElementById("myAudio17");
+function playAudio17() {
+    ii.play();
+}
+var j = document.getElementById("myAudio18");
+function playAudio18() {
+    j.play();
+}
+var jj = document.getElementById("myAudio19");
+function playAudio19() {
+    jj.play();
+}
+var k = document.getElementById("myAudio20");
+function playAudio20() {
+    k.play();
+}
+var kk = document.getElementById("myAudio21");
+function playAudio21() {
+    kk.play();
+}
+var l = document.getElementById("myAudio22");
+function playAudio22() {
+    l.play();
+}
+var ll = document.getElementById("myAudio23");
+function playAudio23() {
+    ll.play();
+}
+var m = document.getElementById("myAudio24");
+function playAudio24() {
+    m.play();
+}
+var mm = document.getElementById("myAudio25");
+function playAudio25() {
+    mm.play();
+}
+var n = document.getElementById("myAudio26");
+function playAudio26() {
+    n.play();
+}
+var nn = document.getElementById("myAudio27");
+function playAudio27() {
+    nn.play();
+}
+var o = document.getElementById("myAudio28");
+function playAudio28() {
+    o.play();
+}
+var oo = document.getElementById("myAudio29");
+function playAudio29() {
+    oo.play();
+}
+var p = document.getElementById("myAudio30");
+function playAudio30() {
+    p.play();
+}
+var pp = document.getElementById("myAudio31");
+function playAudio31() {
+    pp.play();
+}
+var q = document.getElementById("myAudio32");
+function playAudio32() {
+    q.play();
+}
+var qq = document.getElementById("myAudio33");
+function playAudio33() {
+    qq.play();
+}
+var r = document.getElementById("myAudio34");
+function playAudio34() {
+    r.play();
+}
+var rr = document.getElementById("myAudio35");
+function playAudio35() {
+    rr.play();
+}
+var s = document.getElementById("myAudio36");
+function playAudio36() {
+    s.play();
+}
+var ss = document.getElementById("myAudio37");
+function playAudio37() {
+    ss.play();
+}
+var t = document.getElementById("myAudio38");
+function playAudio38() {
+    t.play();
+}
+var tt = document.getElementById("myAudio39");
+function playAudio39() {
+    tt.play();
+}
+var u = document.getElementById("myAudio40");
+function playAudio40() {
+    u.play();
+}
+var uu = document.getElementById("myAudio41");
+function playAudio41() {
+    uu.play();
+}
+var v = document.getElementById("myAudio42");
+function playAudio42() {
+    v.play();
+}
+var vv = document.getElementById("myAudio43");
+function playAudio43() {
+    vv.play();
+}
+var w = document.getElementById("myAudio44");
+function playAudio44() {
+    w.play();
+}
+var ww = document.getElementById("myAudio45");
+function playAudio45() {
+    ww.play();
+}
+var x = document.getElementById("myAudio46");
+function playAudio46() {
+    x.play();
+}
+var xx = document.getElementById("myAudio47");
+function playAudio47() {
+    xx.play();
+}
+var y = document.getElementById("myAudio48");
+function playAudio48() {
+    y.play();
+}
+var yy = document.getElementById("myAudio49");
+function playAudio49() {
+    yy.play();
+}
+var z = document.getElementById("myAudio50");
+function playAudio50() {
+    z.play();
+}
+var zz = document.getElementById("myAudio51");
+function playAudio51() {
+    zz.play();
+}
